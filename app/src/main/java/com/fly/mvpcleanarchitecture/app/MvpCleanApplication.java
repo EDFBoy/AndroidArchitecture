69e0388ba11074db0a8362a00f6369adc096f687
baofly <904498910@qq.com>
2017-03-08 15:07:28 +0800

    'update'

--- app/src/main/java/com/fly/mvpcleanarchitecture/app/MvpCleanApplication.java
+++ app/src/main/java/com/fly/mvpcleanarchitecture/app/MvpCleanApplication.java
@@ -1,17 +1,20 @@
 package com.fly.mvpcleanarchitecture.app;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+import com.fly.mvpcleanarchitecture.BuildConfig;
 import com.fly.mvpcleanarchitecture.app.config.Constants;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApiServiceModule;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApplicationComponent;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApplicationModule;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.DaggerApplicationComponent;
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
 import org.litepal.LitePal;
 
 /**
  * Created by Administrator on 2016/8/3.
  */
-public class MvpCleanApplication extends Application {
+public class MvpCleanApplication extends MultiDexApplication {
 
     private static ApplicationComponent applicationComponent;
 
@@ -21,6 +24,11 @@ public class MvpCleanApplication extends Application {
         applicationComponent = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this)).apiServiceModule(new
                 ApiServiceModule(Constants.ServerUrl)).build();
         LitePal.initialize(this);
+        if (BuildConfig.IS_DEBUG) {
+            Logger.init(BuildConfig.LOG_TAG).logLevel(LogLevel.FULL);
+        } else {
+            Logger.init(BuildConfig.LOG_TAG).logLevel(LogLevel.NONE);
+        }
     }
 
     public static ApplicationComponent getApplicationComponent() {
